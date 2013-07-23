49588b7fd993a126d235df42cf5129899489e212
thehonestape <abe@thehonestape.com>
2013-07-23 02:57:39 -0400

    Setting Up Soundcloud
    Set up soundcloud player javascript

---
+++ public/js/what.js
@@ -0,0 +1,13 @@
+SC.initialize({
+    client_id: "86c13cfdb42be2cadcceaebea606e67a",
+});
+
+$(document).ready(function() {
+  SC.oEmbed("https://soundcloud.com/fleezyfleez/fly-society-soul-20-20-remix", 
+  	{color: "05273A", 
+  		show_artwork: "false", 
+  		buying: "false", 
+  		show_comments: 'false',
+  		default_height: '30px'},  
+  	document.getElementById("soundcloud-player"));
+  });
