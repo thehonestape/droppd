b0e921934131245b576154f43d3c8e920241adeb
thehonestape <abe@thehonestape.com>
2013-07-23 05:12:02 -0400

    More Port Adjustments to Fix Heroku Crash Bug
    .

--- app.js
+++ app.js
@@ -26,5 +26,6 @@ app.get('/', function (req, res) {
   { title : 'Home' }
   )
 })
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
