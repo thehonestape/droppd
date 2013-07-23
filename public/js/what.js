6bdda38de5551f47b7726a26605b9ed8bd51e938
thehonestape <abe@thehonestape.com>
2013-07-23 03:34:14 -0400

    Test Of Branch and Sync
    Having some github toubles. Testing a branch merge w/ this small
    commit.

--- public/js/what.js
+++ public/js/what.js
@@ -1,7 +1,6 @@
 SC.initialize({
     client_id: "86c13cfdb42be2cadcceaebea606e67a",
 });
-
 $(document).ready(function() {
   SC.oEmbed("https://soundcloud.com/fleezyfleez/fly-society-soul-20-20-remix", 
   	{color: "05273A", 
