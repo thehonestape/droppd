a28328cf380dc88e64e065eb41a84191ab51c48f
thehonestape <abe@thehonestape.com>
2013-07-29 04:18:37 -0400

    Soundcloud API Tweaks and Audio Player Attempts
    some attempts at a custom soundcloud player. nothing worked out but
    will keep files here.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
   res.render('index',
-  { title : 'Home' }
+  { title : 'Fleezus' }
   )
 })
 app.listen(process.env.PORT || 3000, function(){
--- public/js/what.js
+++ public/js/what.js
@@ -10,3 +10,54 @@ $(document).ready(function() {
   		default_height: '30px'},  
   	document.getElementById("soundcloud-player"));
   });
+
+
+soundManager.setup({
+  url: '/../swf',
+  flashVersion: 9, // optional: shiny features (default = 8)
+  // optional: ignore Flash where possible, use 100% HTML5 mode
+  // preferFlash: false,
+  onready: function() {
+    console.log('this thing might be working');
+    // Ready to use; soundManager.createSound() etc. can now be called.
+  }
+});
+
+
+// 360player.js, config section
+
+this.config = {
+
+  playNext: false,
+  autoPlay: false,
+  allowMultiple: false,
+  loadRingColor: '#F04C3D',
+  playRingColor: '#F04C3D',
+  backgroundRingColor: '#F04C3D',
+  circleDiameter: 256,
+  circleRadius: 128,
+  animDuration: 500,
+  animTransition: Animator.tx.bouncy,
+  showHMSTime: true,
+
+  useWaveformData: true,
+  waveformDataColor: '#F04C3D',
+  waveformDataDownsample: 7,
+  waveformDataOutside: false,
+  waveformDataConstrain: false,
+  waveformDataLineRatio: 1.04,
+
+  useEQData: true,
+  eqDataColor: '#F04C3D',
+  eqDataDownsample: 7,
+  eqDataOutside: true,
+  eqDataLineRatio: 0.69,
+
+  usePeakData: true,
+  peakDataColor: '#ff33ff',
+  peakDataOutside: true,
+  peakDataLineRatio: 0.5,
+
+  useAmplifier: true
+
+}
