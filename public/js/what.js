46c844a7906a25a9922338ba4af8cc667ae46e8c
thehonestape <abe@thehonestape.com>
2013-07-31 11:58:44 -0400

    Reverted to old soundcloud player
    new player/old player

--- public/js/what.js
+++ public/js/what.js
@@ -1,20 +1,13 @@
+SC.initialize({
+    client_id: "86c13cfdb42be2cadcceaebea606e67a",
+});
 $(document).ready(function() {
-     var widget = SC.Widget(document.getElementById('soundcloud-widget'));
-     widget.bind(SC.Widget.Events.READY, function() {
-       console.log('Ready...');
-     });
-     $('.play').click(function() {
-       widget.toggle();
-     });  
-
-	 $('.next').click(function() {
-	  	widget.next();
-	 }); 	
-
-	 $('.prev').click(function() {
-	  	widget.prev(); 	
-   	});
-      });
-
-
-
+  SC.oEmbed("https://soundcloud.com/casualsophistication/sets/jettin-fly-society-mic-flo", 
+  	{color: "05273A", 
+  		show_artwork: "false", 
+  		buying: "false", 
+  		show_comments: 'false',
+  		default_height: '30px'},  
+  	document.getElementById("soundcloud-player"));
+  });
+  
