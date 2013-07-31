6540b1700dd347907ee5776bf363b089663bca58
thehonestape <abe@thehonestape.com>
2013-07-31 02:47:04 -0400

    Updated player and player controls
    player controls updated

--- public/js/what.js
+++ public/js/what.js
@@ -1,14 +1,20 @@
-SC.initialize({
-    client_id: "86c13cfdb42be2cadcceaebea606e67a",
-});
 $(document).ready(function() {
-  SC.oEmbed("https://soundcloud.com/fleezyfleez/fly-society-soul-20-20-remix", 
-  	{color: "05273A", 
-  		show_artwork: "false", 
-  		buying: "false", 
-  		show_comments: 'false',
-  		default_height: '30px'},  
-  	document.getElementById("soundcloud-player"));
-  });
+     var widget = SC.Widget(document.getElementById('soundcloud-widget'));
+     widget.bind(SC.Widget.Events.READY, function() {
+       console.log('Ready...');
+     });
+     $('.play').click(function() {
+       widget.toggle();
+     });  
+
+	 $('.next').click(function() {
+	  	widget.next();
+	 }); 	
+
+	 $('.prev').click(function() {
+	  	widget.prev(); 	
+   	});
+      });
+
 
 
